f1fb5c09891e8125809c0a68d5b3806112c94e2a
MYKHAILO LASKAVYI <110311691+LASKAV@users.noreply.github.com>
2024-01-31 12:12:10 +0100

    Add files via upload

---
+++ Ragnarok/Ragnarok/RagnarokApp.swift
@@ -0,0 +1,17 @@
+//
+//  RagnarokApp.swift
+//  Ragnarok
+//
+//  Created by LASKAV on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RagnarokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
